3a2db0b96af2bc2bfb570e86932b702b190875fe
jenifaa <jefrahman32@gmail.com>
2025-02-14 23:46:09 +0600

    added navbar,footer and header

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import { FiPhoneCall } from "react-icons/fi";
+import { CiClock2 } from "react-icons/ci";
+import { MdOutlineEmail } from "react-icons/md";
+import { CiLocationArrow1 } from "react-icons/ci";
+const Header = () => {
+  return (
+    <div className="flex gap-28 items-center border-b">
+      <div className="flex w-2/5  bg-blue-900 text-white items-center gap-5  rounded-tr-[10%_90%] px-5 py-3 ">
+        <p>Fast, <span className="font-semibold text-blue-200">Accurate</span> Lab Services.</p>
+        <p className="flex items-center gap-1">
+         <span className="flex justify-center items-center bg-blue-700 w-8 h-8 rounded-full"> <FiPhoneCall /></span> +012345678910
+        </p>
+      </div>
+      <div className="w-3/5 flex items-center  text-sm justify-end px-8">
+        <p className="flex items-center gap-1"><CiClock2 className="text-xl text-blue-500 font-semibold"></CiClock2>Mon-Sat 9am-10pm, Sun-closed |</p>
+        <p className="ml-1 flex items-center gap-1"> <MdOutlineEmail className="text-xl text-blue-500 "></MdOutlineEmail> labsync@gmail.com |</p>
+        <p className="ml-1 flex items-center gap-1">
+          <CiLocationArrow1 className="text-xl text-blue-500"></CiLocationArrow1>
+          123 Science Avenue, Innovation Park, New York,USA
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+import Header from "./Header";
 const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/about">About</Link>
+      </li>
+      <li>
+        <Link href={"/service"}>Services</Link>
+      </li>
+      <li>
+        <Link href="/blog">Blog</Link>
+      </li>
+      <li>
+        <Link href="/appointment">Appointment</Link>
+      </li>
+
+      
+    </>
+  );
+
   return (
     <div>
-      <div className="navbar bg-base-100">
+        <Header></Header>
+      <div className="navbar bg-base-100 px-10">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -24,52 +50,24 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {links}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <Link href={"/"} className=" text-xl">
+            <Image
+              alt="logo"
+              src={"/assets/images/laboratory.png"}
+              width={60}
+              height={60}
+            ></Image>
+            <p className="font-bold">LabSync</p>
+          </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
+          <ul className="menu menu-horizontal px-1">{links}</ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <a className="text-xl font-bold text-blue-600">Sign In</a>
         </div>
       </div>
     </div>
